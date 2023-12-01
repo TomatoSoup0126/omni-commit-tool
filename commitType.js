661e10acd4b11c33b7eb7a556587e978682d6845
Jonathan Tang <jonathan.tang@omnichat.ai>
2023-12-01 13:55:49 +0800

    feat: 🎸 add steps and commit type

---
+++ commitType.js
@@ -0,0 +1,68 @@
+export default [
+	{
+		name: 'feat',
+		emoji: '💡',
+		description: 'A new feature',
+		value: 'feat'
+	},
+	{
+		name: 'style',
+		emoji: '🎨',
+		description: 'Markup, white-space, formatting, missing semi-colons...',
+		value: 'style'
+	},
+	{
+		name: 'fix',
+		emoji: '🐛',
+		description: 'A bug fix',
+		value: 'fix'
+	},
+	{
+		name: 'hotfix',
+		emoji: '🚨',
+		description: 'Emergency fix',
+		value: 'hotfix'
+	},
+	{
+			name: 'chore',
+			emoji: '🧹',
+			description: 'Build process or auxiliary tool changes',
+			value: 'chore'
+	},
+	{
+		name: 'refactor',
+		emoji: '🔨',
+		description: 'A code change that neither fixes a bug or adds a feature',
+		value: 'refactor'
+	},
+	{
+		name: 'perf',
+		emoji: '⚡',
+		description: 'A code change that improves performance',
+		value: 'perf'
+	},
+	{
+		name: 'test',
+		emoji: '🎮',
+		description: 'Adding missing tests',
+		value: 'test'
+	},
+	{
+		name: 'ci',
+		emoji: '👷',
+		description: 'CI related changes',
+		value: 'ci'
+	},
+	{
+		name: 'docs',
+		emoji: '📝',
+		description: 'Documentation only changes',
+		value: 'docs'
+	},
+	{
+		name: 'release',
+		emoji: '🎉',
+		description: 'Create a release commit',
+		value: 'release'
+	}
+]
--- index.js
+++ index.js
@@ -1,14 +1,104 @@
 #!/usr/bin/env node
+import prompts from 'prompts'
+import chalk from 'chalk'
+import commitType from './commitType.js'
 
-const prompts = require('prompts');
+const defaultJiraPrefix = 'OCPD'
 
-(async () => {
-	const response = await prompts({
-		type: 'number',
-		name: 'value',
-		message: 'How old are you?',
-		validate: value => value < 18 ? `Nightclub is 18+ only` : true
-	});
+const commitTypesList = commitType.map(type => ({
+  title: type.name,
+  description: `${type.emoji} ${type.description}`,
+  value: type.value,
+  emoji: type.emoji
+}))
 
-  console.log(response); // => { value: 24 }
+const choose_type = {
+  type: 'autocomplete',
+  name: 'commitTypeValue',
+  message: 'Pick a commit type.',
+  choices: commitTypesList,
+  fallback: 'No matched type.'
+}
+
+const input_message = {
+  type: 'text',
+  name: 'commit_message',
+  message: prev => {
+    const target = commitTypesList.find(type => type.value === prev)
+    return `${target.emoji} ${target.title}`
+  },
+  validate: value => {
+    if (!value) {
+      return 'Commit message is required.'
+    }
+    return true
+  }
+}
+
+const is_jira = {
+  type: 'confirm',
+  name: 'is_jira',
+  message: 'Tag Jira issue ?',
+  initial: true
+}
+
+const input_jira = {
+	type: prev => prev ? 'number' : null,
+	name: 'jira_id',
+	message: 'Jira issue id',
+	onRender () {
+    this.msg = chalk.bgBlueBright.white(' Jira issue ID ')
+  },
+	validate: value => {
+		if (!value) {
+			return 'Jira issue id is required.'
+		}
+		return true
+	}
+}
+
+const input_category = {
+  type: 'text',
+  name: 'commit_category',
+  message: 'Commit Category',
+  validate: value => {
+    if (!value) {
+      return 'Commit Category is required.'
+    }
+    return true
+  }
+}
+
+const steps = [
+	choose_type,
+	input_message,
+	is_jira,
+	input_jira,
+	input_category
+]
+
+;(async () => {
+	let isCanceled = false
+
+	const response = await prompts(steps, {
+    onSubmit: (prompt, answers) => {
+      if (answers === undefined) {
+        isCanceled = true
+        return true
+      }
+    },
+    onCancel: (prompt) => {
+      isCanceled = true
+      return false
+    }
+  });
+
+  console.log(response)
+	const {
+		commitTypeValue,
+		commit_message,
+		jira_id,
+		commit_category
+	} = response
+	console.log(`[${defaultJiraPrefix}-${jira_id}] ${commitTypeValue}(${commit_category}): ${commit_message}`)
 })();
