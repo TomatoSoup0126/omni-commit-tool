a876b19cd9c5a1dded4657f918a67a84b9d819bc
Jonathan Tang <jonathan.tang@omnichat.ai>
2023-12-04 13:33:18 +0800

    feat: 🎸 add try catch for cancel handler

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const defaultJiraPrefix = 'OCPD'
 
 	const response = await prompts(steps, {
     onSubmit: (prompt, answers) => {
+      console.log('answers', answers)
       if (answers === undefined) {
         isCanceled = true
         return true
@@ -21,17 +22,25 @@ const defaultJiraPrefix = 'OCPD'
     }
   });
 
-  console.log(response)
-	const {
-		commit_type_value,
-		commit_message,
-    is_jira,
-		jira_id,
-		issue_category
-	} = response
+  try {
+    if (isCanceled) {
+      throw new Error('Abort commit')
+    }
+    console.log(response)
+    const {
+      commit_type_value,
+      commit_message,
+      is_jira,
+      jira_id,
+      issue_category
+    } = response
 
-  const jiraString = is_jira && `[${defaultJiraPrefix}-${jira_id}]`
-  const commitString = `${commitType.find(item => item.name === commit_type_value)?.emoji}${commit_type_value}`
-  const categoryString = issue_category && `(${issue_category})`
-	console.log(`${jiraString} ${commitString}${ categoryString }: ${commit_message}`)
+    const jiraString = is_jira ? `[${defaultJiraPrefix}-${jira_id}]` : ''
+    const commitString = `${commitType.find(item => item.name === commit_type_value)?.emoji} ${commit_type_value}`
+    const categoryString = !!issue_category ? `(${issue_category})` : ''
+    console.log(`${jiraString} ${commitString}${ categoryString }: ${commit_message}`)
+  } catch (error) {
+    console.log('Abort commit')
+  }
+
 })();
