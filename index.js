39c8460c82c5274ba3060a2e3602310d4635739b
Jonathan Tang <jonathan.tang@omnichat.ai>
2023-12-12 18:04:20 +0800

    💡 feat: show type list command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import { launchCommitPrompt } from './src/commit/commitPrompts.js'
 import { launchConfigPrompt } from './src/config/config.js'
 import { showConfigPath } from './src/utils/showConfigPath.js'
+import { showCommitTypes } from './src/commit/showTypeList.js'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
@@ -16,9 +17,13 @@ const argv = yargs(hideBin(process.argv))
     alias: 's',
     describe: 'Set config'
   },
-  'path': {
-    alias: 'p',
+  'config': {
+    alias: 'c',
     describe: 'Show config path'
+  },
+  'types': {
+    alias: 't',
+    describe: 'Show commit type list path'
   }
 })
 .help()
@@ -26,10 +31,12 @@ const argv = yargs(hideBin(process.argv))
 
 if (argv.setting) {
   launchConfigPrompt()
-} else if (argv.path) {
+} else if (argv.config) {
   showConfigPath()
 } else if (argv.blank) {
   launchCommitPrompt({ blank: true })
+} else if (argv.types) {
+  showCommitTypes()
 } else {
   launchCommitPrompt()
 }
